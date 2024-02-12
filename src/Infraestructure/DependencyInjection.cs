53b4f2c12e3959f8458e651fea7516260565d83f
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2024-02-12 18:10:12 +0100

    Infraestructure layer

---
+++ src/Infraestructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infraestructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
