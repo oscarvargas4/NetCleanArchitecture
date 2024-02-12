809d8b3512237688be485e243fba33fba1a0528e
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2024-02-12 18:16:47 +0100

    Presentation Layer and WebApi

---
+++ src/Presentation/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
